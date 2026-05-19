f1514b4d2956e63c8de5fb6e632c024d7aa86943
Sebastian Cursaro <112600689+scursaro@users.noreply.github.com>
2026-05-19 23:27:16 +0000

    added notes and likes

---
+++ notes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Notes(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveSmallIntegerField(default=0)
+
+
+
+# Create your models here.
