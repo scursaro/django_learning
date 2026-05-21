90945bbeb344033e6f61dc7a5d688535cf6b4455
Sebastian Cursaro <112600689+scursaro@users.noreply.github.com>
2026-05-21 19:40:44 +0000

    added notes details

--- notes/views.py
+++ notes/views.py
@@ -6,4 +6,9 @@ def list(request):
     all_notes = Notes.objects.all()
     return render(request, "notes/notes_list.html", {"notes": all_notes})
 
+
+def detail(request, pk):
+    note = Notes.objects.get(pk=pk)
+    return render(request, "notes/notes_detail.html", {"note":note})
+
 # Create your views here.
