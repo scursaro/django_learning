de2038903c6902fabadf9246f1abb632cf10fd18
Sebastian Cursaro <112600689+scursaro@users.noreply.github.com>
2026-05-21 19:14:10 +0000

    added notes visualization

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Notes
+
+def list(request):
+    all_notes = Notes.objects.all()
+    return render(request, "notes/notes_list.html", {"notes": all_notes})
+
 # Create your views here.
--- smartnotes/urls.py
+++ smartnotes/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("home.urls"))
+    path("", include("home.urls")),
+    path('smart/', include('notes.urls')),
   
 ]
